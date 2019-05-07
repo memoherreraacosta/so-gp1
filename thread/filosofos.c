3171ce5d19e9476d125b8ae62028153e26597360
Guillermo Herrera <a01400835@itesm.mx>
2019-05-06 20:57:14 -0500

    Filosofos terminado

--- thread/filosofos.c
+++ thread/filosofos.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <time.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 #define NUM_FILO 5
 #define NUM_FORKS 5
 
-semaphore_t forks[NUM_FORKS]
+sem_t forks[NUM_FORKS];
+
+long randomInRange(int start, int end){
+	return (start + random()%(end-start));	
+}
 
 void * life(void *td){
 	long id = (long)td;
+	srandom((id+1)*time(NULL));
+	printf("Soy el filosofo [ %ld ]\n",id);	
 	for(;;){
-		// pensar
+		// pensar -- Procesar
+		printf("%ld> Voy a pensar \n",id);
+		sleep(randomInRange(5,10));
+		printf("%ld> Tengo hambre \n",id);
 		if(id % 2 == 0){
 			sem_wait(&forks[id]);
 			sem_wait(&forks[(id + 1)%NUM_FILO]);
@@ -18,22 +31,26 @@ void * life(void *td){
 			sem_wait(&forks[id]);
 		}
 
-		// comer 
+		// comer -- Guardar
+		printf("%ld> Voy a comer \n",id);
+		sleep(randomInRange(2,6));
+		sem_wait(&forks[id]);
+                sem_wait(&forks[(id + 1)%NUM_FILO]);
 	}
 }	
 
 int main(int argc, char ** argv){
-	pthread_t threads[NUM_THREAD];
+	pthread_t filosofos[NUM_FILO];
 	long t;
-	for (t = 0 ; t < NUM_THREAD ; t++){
-		pthread_create(&threads[t],NULL, printHello,(void *)t);
-	}
 	
-	for (t = 0 ; t < NUM_THREAD ; t++){
-		pthread_join(threads[t],NULL);
+	for( t = 0 ; t < NUM_FILO; t++){
+		sem_init(&forks[t],0,1);
 	}
 
-	printf("Saldo final [ %ld ]\n",saldo);
+	for (t = 0 ; t < NUM_FILO ; t++){
+		pthread_create(&filosofos[t],NULL, life,(void *)t);
+	}
+
 	pthread_exit(NULL);
-	return 0;
+     // return 0;
 }
